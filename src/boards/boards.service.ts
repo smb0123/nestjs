38deecaea3aa8d6a4d4b17f2ef2afd20c60be16e
심민보 <smb5174@naver.com>
2025-08-26 13:31:51 +0900

    feat: 게시물에 대한 CRUD 및 유효성 검사 추가

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
-
+import { v1 as uuid } from 'uuid';
+import { Board, BoardStatus } from './board.model';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+  private boards: Board[] = [];
+
+  getAllBoards(): Board[] {
+    return this.boards;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
+
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+
+    return board;
+  }
+
+  getBoardById(id: string): Board | undefined {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board | undefined {
+    const board = this.getBoardById(id);
+    if (board) {
+      board.status = status;
+    }
+
+    return board;
+  }
+}
