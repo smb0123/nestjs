263ebe8165b33d5430a32a2c28f9ec82ea6cab5e
심민보 <smb5174@naver.com>
2025-08-28 13:42:20 +0900

    feat: board 관련 api에 AuthGuard 적용

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { Board } from './board.entity';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Board])],
+  imports: [TypeOrmModule.forFeature([Board]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
